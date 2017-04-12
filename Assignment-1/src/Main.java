0693032b2f580dde5b81d481b38906fb6f5beadf
U-DONALD\donal <donald.muffler@live.com>
2017-04-11 19:34:21 -0700

    starter gui code

--- Assignment-1/src/Main.java
+++ Assignment-1/src/Main.java
@@ -1,7 +1,5 @@
 import java.awt.EventQueue;
 
-import gui.SnapShopGUI;
-
 /**
  * 
  */
--- Assignment-1/src/User.java
+++ Assignment-1/src/User.java
@@ -3,31 +3,51 @@
  */
 
 /**
- * @author donal
  *
  */
 public class User {
 	private String myName;
 	private String myEmail;
+	
+	/**
+	 * 
+	 * @param theName
+	 * @param theEmail
+	 */
 	public User(final String theName, final String theEmail) {
 		myName = theName;
 		myEmail = theEmail;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getName() {
 		return myName;
 	}
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getEmail() {
 		return myEmail;
 	}
+	
+	/**
+	 * 
+	 * @param theName
+	 */
 	public void setName(String theName) {
 		myName = theName;
 	}
 	
+	/**
+	 * 
+	 * @param theEmail
+	 */
 	public void setEmail(String theEmail) {
 		myEmail = theEmail;
 	}
-	
-	
-	
 }
