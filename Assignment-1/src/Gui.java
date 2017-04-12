8a9639f3fe9e97602643e9c52a4cffcf817354ec
ysalo <slav27@gmail.com>
2017-04-11 20:10:43 -0700

    added centering

--- Assignment-1/src/Gui.java
+++ Assignment-1/src/Gui.java
@@ -1,4 +1,5 @@
 import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
@@ -13,6 +14,18 @@ import javax.swing.JMenuBar;
  */
 public class Gui extends JFrame {
 
+    /** ToolKit. */
+    private static final Toolkit KIT = Toolkit.getDefaultToolkit();
+    
+    /** Dimensions of the screen. */
+    private static final Dimension SCREEN_SIZE = KIT.getScreenSize();
+    
+    /** The width of the screen. */
+    private static final int SCREEN_WIDTH = SCREEN_SIZE.width;
+    
+    /** The height of the screen. */
+    private static final int SCREEN_HEIGHT = SCREEN_SIZE.height;
+	
 	/**
 	 * 
 	 */
@@ -35,6 +48,7 @@ public class Gui extends JFrame {
 		setJMenuBar(myMenu);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
+		setCentered();
 		setVisible(true);
 	}
 	
@@ -47,4 +61,10 @@ public class Gui extends JFrame {
 		
 		myMenu.add(fileMenu);
 	}
+    private void setCentered() {
+        
+        setLocation(SCREEN_WIDTH / 2 - getWidth() / 2, 
+                            SCREEN_HEIGHT / 2 - getHeight() / 2);
+    }
+    
 }
