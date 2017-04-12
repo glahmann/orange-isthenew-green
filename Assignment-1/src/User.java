a9729a3892f5075b66dab08d6bf685548eb73b13
ysalo <slav27@gmail.com>
2017-04-11 19:32:02 -0700

    add java docs comments

--- Assignment-1/src/User.java
+++ Assignment-1/src/User.java
@@ -9,22 +9,48 @@
 public class User {
 	/** The name of the user. */
 	private String myName;
+	
+	/** The email of the user. */
 	private String myEmail;
+	
+	/**
+	 * Initialize instance fields. 
+	 * 
+	 * @param theName the name of the user.
+	 * @param theEmail the email of the user.
+	 */
 	public User(final String theName, final String theEmail) {
 		myName = theName;
 		myEmail = theEmail;
 	}
 	
+	/**
+	 * Gets the name of the user.
+	 * @return the name of the user.
+	 */
 	public String getName() {
 		return myName;
 	}
+	
+	/**
+	 * Gets the email of the user. 
+	 * @return the email of the user.
+	 */
 	public String getEmail() {
 		return myEmail;
 	}
+	/**
+	 * Sets the name of the user. 
+	 * @param theName the name to set to.
+	 */
 	public void setName(String theName) {
 		myName = theName;
 	}
 	
+	/**
+	 * Sets the email of the user.
+	 * @param theEmail the email to set to.
+	 */
 	public void setEmail(String theEmail) {
 		myEmail = theEmail;
 	}
