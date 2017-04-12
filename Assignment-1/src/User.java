b0c3d579014f79e3f8123f9a05a2e677a4d8616b
ysalo <slav27@gmail.com>
2017-04-11 20:02:01 -0700

    fix comments

--- Assignment-1/src/User.java
+++ Assignment-1/src/User.java
@@ -13,16 +13,10 @@ public class User {
 	private String myEmail;
 	
 	/**
-<<<<<<< HEAD
-	 * 
-	 * @param theName
-	 * @param theEmail
-=======
 	 * Initialize instance fields. 
 	 * 
 	 * @param theName the name of the user.
 	 * @param theEmail the email of the user.
->>>>>>> test
 	 */
 	public User(final String theName, final String theEmail) {
 		myName = theName;
@@ -30,53 +24,33 @@ public class User {
 	}
 	
 	/**
-<<<<<<< HEAD
-	 * 
-	 * @return
-=======
 	 * Gets the name of the user.
 	 * @return the name of the user.
->>>>>>> test
 	 */
 	public String getName() {
 		return myName;
 	}
-	
+
 	/**
-<<<<<<< HEAD
-	 * 
-	 * @return
-=======
+
 	 * Gets the email of the user. 
 	 * @return the email of the user.
->>>>>>> test
 	 */
 	public String getEmail() {
 		return myEmail;
 	}
-<<<<<<< HEAD
-	
-	/**
-	 * 
-	 * @param theName
-=======
+
 	/**
 	 * Sets the name of the user. 
 	 * @param theName the name to set to.
->>>>>>> test
 	 */
 	public void setName(String theName) {
 		myName = theName;
 	}
 	
 	/**
-<<<<<<< HEAD
-	 * 
-	 * @param theEmail
-=======
 	 * Sets the email of the user.
 	 * @param theEmail the email to set to.
->>>>>>> test
 	 */
 	public void setEmail(String theEmail) {
 		myEmail = theEmail;
